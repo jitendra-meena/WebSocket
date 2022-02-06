572aaf5877d8f22edaee54e1e6786f8c86955676
Jitendra Meena <jitendrameena9575@gmail.com>
2022-02-06 21:58:39 +0530

    whatsapp chat

--- websocket/accounts/views.py
+++ websocket/accounts/views.py
@@ -1,10 +1,5 @@
-from email import message
 from django.shortcuts import redirect, render
-from .forms import SignUpForm
-from django.contrib.auth import get_user_model
 from django.contrib.auth import authenticate, login, logout
-from django.http import HttpResponseRedirect, HttpResponse
-from django.urls import reverse
 from django.views import View
 from django.contrib import messages
 from django.contrib.auth.models import User
@@ -63,64 +58,4 @@ class Login(View):
             messages.error(request, f"NO ACCOUNT FOUND WITH USERNAME {username} AND PASSWORD {password}")
             return render(request,'accounts/login.html')    
                 
-            
-    
 
-
-
-
-
-
-
-
-
-
-# # Create your views here.
-
-# User = get_user_model()
-
-
-# def register(request):
-#     if request.method == 'POST':
-#         form = SignUpForm(request.POST)
-
-#         if form.is_valid():
-#             new_user = form.save()
-#             new_user = authenticate(username=form.cleaned_data['username'],
-#                                     password=form.cleaned_data['password1'],
-#                                     )
-#             login(request, new_user)
-
-#             return HttpResponseRedirect(reverse('home'))
-
-#     else:
-#         form = SignUpForm()
-
-#     return render(request, 'accounts/register.html', {'form': form})
-
-
-# def loginView(request):
-#     if request.user.is_authenticated:
-#         return HttpResponseRedirect(reverse('home'))
-
-#     if request.method == 'POST':
-#         username = request.POST.get('username')
-#         password = request.POST.get('password')
-
-#         user = authenticate(request, username=username, password=password)
-
-#         if user:
-#             if user.is_active:
-#                 login(request, user)
-#                 return HttpResponseRedirect(reverse('home'))
-#             else:
-#                 return HttpResponse("Account Not Active")
-#         else:
-#             context = {'notfound': True}
-#             print(
-#                 f"NO ACCOUNT FOUND WITH USERNAME {username} AND PASSWORD {password}")
-#             print(context)
-#             return render(request, 'accounts/login.html', context)
-
-#     else:
-#         return render(request, 'accounts/login.html')
